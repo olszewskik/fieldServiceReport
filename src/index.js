1986606144e21e92f13ec39b728f8dff080f7f0a
Kamil Olszewski <kamil.web@gmail.com>
2019-10-09 22:53:19 +0200

    organizing the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
 
 
 ReactDOM.render(
-    <h1>Field Service Report App</h1>,
+  <App />,
 
-    document.getElementById('root')
+  document.getElementById('root'),
 );
-
-
-
