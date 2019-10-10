ef3f42cf0b5e06fb174841c02d63bb480a3c6871
Kamil Olszewski <kamil.web@gmail.com>
2019-10-10 20:25:04 +0200

    organizing the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 const App = () => (
-<<<<<<< HEAD
-  <h1>Report Service App</h1>
+  <h1>
+    Item Report Service Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu eros,
+    consectetur a est sit amet, rutrum finibus augue. Pellentesque luctus, risus sed maximus
+    posuere, dolor nisl bibendum diam, et laoreet magna magna eget orci. Nulla turpis nunc, rhoncus
+    sagittis purus et, hendrerit convallis odio. Aenean vel dolor vitae nulla sollicitudin commodo.
+    Sed rutrum ut erat bibendum tristique. Etiam sagittis odio sed ullamcorper sollicitudin. Integer
+    ornare, tortor ut facilisis sagittis, purus purus feugiat metus, sed molestie nibh nunc non
+    mauris. Nullam lobortis interdum dapibus. Praesent iaculis tincidunt neque vitae dignissim. Sed
+    cursus tincidunt nisi. Nunc eget bibendum justo. Orci varius natoque penatibus et magnis dis
+    parturient montes, nascetur ridiculus mus. Fusce in lectus urna. Donec lacinia iaculis rutrum.
+    Donec risus elit, porta at condimentum id, vehicula ut lorem.
+  </h1>
 );
 
 export default App;
-=======
-    <h1>Report Service App</h1>
-);
-
-export default App;
->>>>>>> origin/master
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
 
-
 ReactDOM.render(
   <App />,
 
