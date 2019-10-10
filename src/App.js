f250bab29a3c6411aee134738adde89921d95c85
Kamil Olszewski <kamil.web@gmail.com>
2019-10-10 20:25:04 +0200

    organizing the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
 
 const App = () => (
-<<<<<<< HEAD
   <h1>Report Service App</h1>
 );
 
 export default App;
-=======
-    <h1>Report Service App</h1>
-);
-
-export default App;
->>>>>>> origin/master
