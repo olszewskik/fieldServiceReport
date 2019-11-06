7ae1c5d3f15efa335986d4c5ad6925ee8d9aba02
Kamil Olszewski <kamil.web@gmail.com>
2019-11-06 22:26:16 +0100

    .

--- src/views/Reports.js
+++ src/views/Reports.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Reports = () => <h1>Reports</h1>;
+class Reports extends Component {
+  state = {
+    reports: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3000/api/report/reports')
+      .then(response => {
+        this.setState({ reports: response.data.reports });
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Test API</h1>
+        <ul>
+          {this.state.reports.map(report => (
+            <li key={report._id}>{report._id}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
 
 export default Reports;
